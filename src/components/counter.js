07576def8aa228f3e844a97a9eadd77072173bb7
JRP <jithinperingala@gmail.com>
2023-05-03 21:03:19 +0300

    state

--- src/components/counter.js
+++ src/components/counter.js
@@ -3,6 +3,8 @@ import { useState } from "react"
 const Counter = () => {
     let [counter, setCounter] = useState(0);
     let [name, setName] = useState("")
+    let [isListEnabled, setListEnabled] = useState(false)
+    let [employees,setEmployee]=useState(["jithin"])
 
     const incrementCounter = () => {
 
@@ -16,15 +18,40 @@ const Counter = () => {
 
     }
 
+    const checkboxChanged = (event) => {
+        console.log(event.target.checked)
+        setListEnabled(event.target.checked)
+    }
+    const addEmployee=()=>{
+        setEmployee([...employees,name])
+    }
     return (
+        <>
         <div>
             counter={counter} <br />
 
             Name<input type="test" onChange={saveName} /><br />
+            <button onClick={addEmployee}>Add Employee</button><br/>
+            Show List <input type="checkbox" onChange={checkboxChanged} />
+            {
+
+                isListEnabled ?
+                    <ul>
+                        {
+                            employees.map(res=>(
+                                <li>{res}</li>
+                            ))
+                        }
+                       
+
+                    </ul> : <></>
+            }
             {name}<br />
 
             <button onClick={incrementCounter}>+</button>
         </div>
+        <div>uy</div>
+        </>
     )
 }
 export default Counter
